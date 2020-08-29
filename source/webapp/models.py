3f61a328c20bd19594327377b3f84622480613df
Farid <thesomeq@gmail.com>
2020-08-30 05:54:33 +0600

    Этап 1: Добавлена модель опроса, забиты тестовые опросы.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    text = models.TextField(max_length=200, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+        ordering = ('-created_at',)
